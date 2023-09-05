24d52a486fb0450d4f347d404fcce2f858c6cd3c
Wlad loban <qwegadqwe@gmail.com>
2023-09-05 09:44:52 +0300

    feat: common styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import '@fontsource/inter/500.css';
+import '@fontsource/inter/700.css';
+import './styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
